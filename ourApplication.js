0f74c52d7dfc94988d0be41b10f3418f97811f28
seanjeff <sean951120@gmail.com>
2021-03-02 22:02:21 +0800

    Add files via upload
    first update

--- ourApplication.js
+++ ourApplication.js
@@ -1,14 +1,33 @@
-var webProvider;
-if (typeof web3 !== 'undefined') {
-    webProvider = web3.currentProvider;
-    web3 = new Web3(webProvider);
-} else {
-    webProvider = new Web3.providers.HttpProvider('http://localhost:5285');
-    web3 = new Web3(webProvider);
+
+function connectNode() {
+    var option = document.querySelector("#ethereumNode").val();
+    if (typeof web3 !== 'undefined') {
+        web3 = new Web3(web3.currentProvider);
+    } else {
+        web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:5285'));
+
+    }
+
+    showInfo();
 }
 
-var account = web3.eth.getCoinbase();
-document.getElementById("myAccount").innerHTML = "my account is :" + JSON.stringify(account);
+async function showInfo() {
+    var account = await web3.eth.getCoinbase();
+    var balance = await web3.eth.getBalance(account);
+    document.querySelector("#myAccount").innerHTML = "my account is: " + account;
+    document.querySelector("#myBalance").innerHTML = "my account balance is: " + web3.utils.fromWei(balance, "ether") + "ether";
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 
